913cb952f0cf0b700cb65b5eacde3430e89e38ad
Neo Andor <neoandor@gmail.com>
2026-03-12 21:50:11 +0800

    fix: restore About to top nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation";
 
 const links = [
   { href: "/", label: "Home" },
+  { href: "/about", label: "About" },
   { href: "/news", label: "News" },
   { href: "/community", label: "Community" },
 ];
